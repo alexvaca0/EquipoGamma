048ab2cc50bd27050d0a94902f1cfb3db2ee5e59
alexvaca0 <alejandro_vaca0@hotmail.com>
2019-01-31 20:22:41 +0100

    updated script for trying models

--- Modelos_R/initial_models.R
+++ Modelos_R/initial_models.R
@@ -4,24 +4,25 @@
 
 library(caret)
 library(caretEnsemble)
-
+library(doParallel)
 
 
 models <- unique(modelLookup()[modelLookup()$forReg,c(1)])
+
 
 
 df <- read.csv('datos.csv', fileEncoding = 'utf-8', stringsAsFactors = F)
 
 df_numeric <- df[, unlist(lapply(df, is.numeric))]
 
-X <- df[ , ]
+X <- df_numeric[ , ]
 
 
-y <- df[ , ]
+y <- df_numeric[ , ]
 
 
 set.seed(123)
-part.index <- createDataPartition(concrete_rand$strength, 
+part.index <- createDataPartition(y, 
                                   p = 0.8,                         
                                   list = FALSE)
 X_train <- X[part.index, ]
@@ -44,7 +45,7 @@ control <- trainControl(method = "cv", # for “cross-validation”
 set.seed(222)
 model_list <- caretList(X_train,
                         y_train,
-                        trControl = my_control,
+                        trControl = control,
                         methodList = models,
                         tuneList = NULL,
                         continue_on_fail = TRUE, 
@@ -56,11 +57,11 @@ models_results <- data.frame(model = models)
 
 min_error <- c()
 
-for (model in model_list) {
+for (i in 1:length(model_list)) {
   
-  if(all(!is.na(model_list$model$results$RMSE))) {
+  if(all(!is.na(model_list[[i]]$results$RMSE))) {
     
-    min_error <- c(min_error, min(model_list$model$results$RMSE, na.rm=T))
+    min_error <- c(min_error, min(model_list[[i]]$results$RMSE, na.rm=T))
     
   } else {
     
@@ -72,11 +73,11 @@ for (model in model_list) {
 
 min_mae <- c()
 
-for (model in model_list) {
+for (i in 1:length(model_list)) {
   
-  if(all(!is.na(model_list$model$results$MAE))) {
+  if(all(!is.na(model_list[[i]]$results$MAE))) {
     
-    min_mae <- c(min_mae, min(model_list$model$results$MAE, na.rm=T))
+    min_mae <- c(min_mae, min(model_list[[i]]$results$MAE, na.rm=T))
     
   } else {
     
